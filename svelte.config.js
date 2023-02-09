ea2b2e6454e68f9c38e415642f5f7f4f19ba8c96
dawnsix <d4wnfr33z3@gmail.com>
2023-02-09 14:45:26 +1000

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+//import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,5 +18,6 @@ const config = {
     adapter: adapter(),
   },
 };
+
 
 export default config;
