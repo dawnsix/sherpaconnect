2efa28446518c4993359b7f4eff4f728bd4d1cc2
dawnsix <d4wnfr33z3@gmail.com>
2023-02-10 22:51:38 +1000

    add profile retrieval

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -4,4 +4,20 @@ import type { PageServerLoad } from "./$types"
 export const load: PageServerLoad = async({ locals }) => {
     if(!locals.session)
         throw redirect(303, '/')
+
+    let { data: profiles, error: err } = await locals.sb
+        .from('profiles')
+        .select()
+        .eq('username', locals.session.user.user_metadata.username)
+
+        if(err) {
+            return { 
+                data: "engaging in dungoofed protocol"
+            }
+        }
+
+    return {
+        profileData: profiles
+    }    
 }
+
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,1 +1,10 @@
-<p>profile</p>
+<script lang="ts">
+    export let data: any;
+</script>
+
+<h3>User Profile</h3>
+<br />
+
+<p>Username: <span class="text-pink-700">{data.profileData[0].username}</span></p>
+<p>Full Name: <span class="text-pink-700">{data.profileData[0].full_name}</span></p>
+<p>Email: <span class="text-pink-700">{data.profileData[0].email}</span></p>
