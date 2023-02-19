5ec5b70be2f21c6a7d801fde175055ddbfaf3501
dawnsix <d4wnfr33z3@gmail.com>
2023-02-19 10:26:00 +1000

    fix null session handling on posts

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -3,7 +3,7 @@ import { supabaseClient } from '$lib/supabaseClient';
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
 
-    let {data: posts, error: err} = await supabaseClient
+    let {data: posts, error: err} = await locals.sb
         .from('posts')
         .select()
         .order('created_at',  { ascending: false });
